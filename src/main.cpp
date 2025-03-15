8d31ce9c5b80ffe3805b1e328389a1e28f381351
Simon Scoltock <sscoltock@simplemachines.com.au>
2025-03-15 20:46:58 +1300

    Move secrets into separate file

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <mbedtls/sha256.h>
 #include <mbedtls/base64.h>
 #include <WiFiClientSecure.h>
+#include "../secrets/secrets.h"
 
 JsonDocument LoadConfig();
 void ConnectToWifi(String ssid, String password);
@@ -54,12 +55,12 @@ void loop()
 JsonDocument LoadConfig()
 {
   JsonDocument config;
-  config["ssid"] = "scoltock";
-  config["wifi_password"] = "nowireshere";
-  config["gcs_sa_email"] = "stormwater@home-automation-453101.iam.gserviceaccount.com";
-  config["gcs_sa_private_key"] = "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDVtQHHCnybKopa\nKrzSnFkTwYYLMxbfZPieJeKoaE1P31X3EMpC/cNcMV1ISMwjqZIJUHHalcfBiHl4\nKWwbKEj/Eo11gOSSOOjjaZJt3+CCAc93DUVWI+0rXqDEnIbTZrc/goRtqljhPb58\nYTLQk3R8iFEASnqf5cceKW2euctrb4aSJHbTrHuwTRpmXaZ4ZDVFTRmF4Pr5o0qe\nxdJGQJphcjS1gBXs2Vf9K7Um/LwkAqP5QY6As6oVB4hkryel1BW49KR6DVUS5E+b\nhOzMoGKni9usu+JHvkpnKtlbyCC/ZV9lJZ7ip0FhD7FtWqGaYXanB5Tv8+tdKJQe\niwkSJ3a9AgMBAAECggEAEwgyNtQvwa/Zr2sD8uu6oSBIfEHaBR4caBUuqNnVHMpi\ngQTNRocL+W9qA9B74act2sd/xDw3lHw/eRyTRJ3jVlgEIhVPBdisoOgbgs5/04Qu\nwkb7yE6dxhGA1tGrzjLlGfJZMd0MWk7h4njTijC/nIGVWSXI4umXQI/m885PF+um\nkcbOLWo24V9cCVF6DJTxI0LKgNGtFeaj/qFTgT3QF1zEKPSBeqG5RqlwtXNvmM3k\nJy4wl4Iwn/EblXuGDCMQR9nGoN6j3Y+LHunsi8Gjqs1/yadC/niOv+Nqf5xoX5V3\nzTdk2pJTAHlbYPUDKVv6FQmDkwagIMAqF048wmKscQKBgQDs4wgxJaKlME2o9fd4\nAQb/ZCaHpn9kO3oDGLltveSguCLs0SJslN6G/aTiUOqg4jGSN64pnZEKdx8MJOS6\nz7GJf2OiYkZ5C921f89gQM9/AhmQiI790v46oJzgJmv9wGFjXkdHOws2jNLNegz9\nbF0/ABO4dpxSlXj9TEU9KMtSOQKBgQDm8zCJ1OIFL+ranLL3s72LI10DRdAUxtBl\n154S9+4WKfaA2+Sr3BU5rfpW7l0m743f8qRmi5ixo2cR1hQteIz7dmDkgGtlqEQh\n3NplEAHzvwK6D7Ysd5O6wG23zyTT2QQknOlZQdeU5KuatYztySDGKKuBkYXzu61F\nexUsOhc4pQKBgGxmgAstc74v2nnlBLePkMox5EfS8xzE8tKT79a+PO8nFCRWl3Ak\nt6gCTf+ak6PHnnOQs0wr5IFrfXOrNlgeTtnix485dZJS0cQKlrUvM0Sli1lOhFC2\nysu9T8xrCKP38xjrvaZk6H6v9o88uNEHU6xOtreE34gTCmivDEgucMxRAoGAPaQj\nDGa5fNDxSjAQxHRA3uYtaTY095apAMWv9zgdX+ULWhFW01gGgkKhUpqEWmQN5fwJ\nTSVtN9x5IhWVhR1r46IQ8mwkPhnPNYqQ7/B51OjifW68HNo9n9bEcg7jsXd2157Z\n2WztcVwnQT+7wauxB7LLM+X3brpk4OqdGBTZJdECgYB6y+iKrTZAcoC2b4hMkx/N\nUSw3osK834QVe7JuovEDZ+LQWtTJYAEHDMqFFLKavWr2aRVUIo4dd4Df5gmfQrfq\noRw9NlqwhJwSmcIGimx4ZDHGFTCTyjWnzXzRywfbTMm6oQEmWtv6ZRUh3/1Q02JB\nhEsDMnGoohm1Q67m/lLtxg==\n-----END PRIVATE KEY-----\n";
-  config["gcs_project_id"] = "home-automation-453101";
-  config["gcs_api_host"] = "pubsub.googleapis.com";
+  config["ssid"] = SSID;
+  config["wifi_password"] = WIFI_PASSWORD;
+  config["gcs_sa_email"] = GCS_SA_EMAIL;
+  config["gcs_sa_private_key"] = GCS_SA_PRIVATE_KEY;
+  config["gcs_project_id"] = GCS_PROJECT_ID;
+  config["gcs_api_host"] = GCS_PUBSUB_API_HOST;
   return config;
 }
 
